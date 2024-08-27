12501be757be3494ce6f48accb5c331256343a9c
Abhishek V Gopal <abhishekvgopal3@gmail.com>
2024-08-27 11:10:02 +0530

    added contact page

--- client/src/layouts/HomeLayout.vue
+++ client/src/layouts/HomeLayout.vue
@@ -9,7 +9,7 @@
 
         
         <div class="p-5">
-
+            
             <div class="flex items-center justify-between w-full">
                 <button class="flex items-center text-primary rounded-full cursor-pointer" @click="openSidebar">
                     <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none">
